f39dfcc9f66069b13e8e25f9dca63bde455a647c
jenry cacerez guzman <jenrybrown@hotmail.com>
2020-05-19 20:39:47 -0400

    Add project files.

---
+++ Matematica/Operation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matematica
+{
+    public class Operation
+    { 
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+        public int ResulS { get; set; }
+        public int ResulR { get; set; }
+        public int ResulM { get; set; }
+
+        public Operation()
+        {
+            Num1 = 0;
+            Num2 = 0;
+            ResulS = 0;
+            ResulR = 0;
+            ResulM = 0;
+
+        }
+
+    }
+}
